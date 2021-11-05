fe03d47c3cc2468f6faeeff1c49d5e3bdab2427b
Denys <denkacha220406@gmail.com>
2021-11-05 13:06:54 +0200

    Beta desktop version

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".ghostsSwiper", {
     slidesPerView: 1,
     spaceBetween: 30,
     loop: true,
@@ -6,6 +6,17 @@ var swiper = new Swiper(".mySwiper", {
         el: ".swiper-pagination",
         dynamicBullets: true,
     },
+    autoplay: {
+        delay: 3500,
+        stopOnLastSlide: true,
+        disableOnInteraction: false,
+    },
+});
+
+var swiper = new Swiper(".staffSwipper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
     autoplay: {
         delay: 3500,
         stopOnLastSlide: true,
